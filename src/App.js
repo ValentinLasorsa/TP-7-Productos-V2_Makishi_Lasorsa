d0ed02b4163d5d07ea63f39297ea165736d00b03
VALENTIN JULIAN LASORSA NIKCEVICH <46919212@ort>
2023-10-24 10:22:54 -0300

    Casi terminado 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./screens/Home";
+import Contacto from './screens/Contacto'
+import Header from './screens/Header'
+import Persona from "./screens/Persona";
+import Nosotros from "./screens/Nosotros";
+import Productos from "./screens/Productos";
+import Cart from "./screens/Cart";
+import Error from "./screens/Error";
+import CarritoProvider from "./context/carritoContext";
+import ProductoProvider from "./context/ProductoContext";
+//import Producto from "./Components/Producto";
+import CategoriaProvider from "./context/categoriaContext";
+import DetalleProducto from "./screens/DetalleProducto";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+ return (
+   <>
+   <ProductoProvider>
+    <CarritoProvider>
+      <CategoriesProvider>
+          <BrowserRouter>
+            <Routes>
+                  <Route path="/" element={<Header />}>
+                      <Route index element={<Home />}></Route>
+                      <Route path="/productos" element={<Productos/>}></Route>
+                      <Route path="/productos/:productoId" element={<DetalleProducto/>}></Route>
+                      <Route path="/quienes-somos" element={<Nosotros/>}></Route>
+                      <Route path="/quienes-somos/:personaId" element={<Persona/>}></Route>
+                      <Route path="/contacto" element={<Contacto />}></Route>
+                      <Route path="/cart" element={<Cart />}></Route>
+                      <Route path="*" element={<Error404 />}></Route>
+                  </Route>
+            </Routes>
+          </BrowserRouter>
+        </CategoriesProvider>
+      </CarritoProvider>
+    </ProductoProvider>
+   </>
+ );
+};
 
 export default App;
